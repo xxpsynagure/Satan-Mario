f5c0a3c98d82c06a5cdab5c6365f4405b2b0cbaa
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-07-24 21:20:28 +0530

    ladder bug fixed

--- CaptainClaw/Actions.cpp
+++ CaptainClaw/Actions.cpp
@@ -49,27 +49,11 @@ void fallDown(int value)
     glutPostRedisplay();
 }
 
-void platformCollision(Human &human, Platforms &platform)
-{   
-    // (human.bottomRight.x - human.topLeft.x) is the size of the human.
-    bool collideX = ( human.bottomRight.x >= platform.bottomLeft.x+5 && platform.bottomRight.x-5 >= human.bottomLeft.x ); 
-
-    bool collideY = ( human.bottomRight.y >= platform.bottomLeft.y && platform.bottomRight.y >= human.bottomLeft.y );
-
-    bool collided = collideX && collideY;
-    if(collided)
-    {
-        moveDownFlag = 0;
-        // std::cout<<collided<<" - omg\t";
-    }
-    else
-        moveDownFlag = 1;
-}
 
 void ladderCollision(Human &human , Ladder &ladder)
 {
-    bool collideX = ( human.bottomLeft.x >= ladder.bottomLeft.x && human.bottomRight.x <= ladder.bottomRight.x ); 
-    bool collideY = ( human.bottomLeft.y >= ladder.bottomLeft.y && ladder.bottomRight.y <= human.bottomRight.y );
+    bool collideX = ( human.bottomRight.x >= ladder.bottomLeft.x && human.bottomLeft.x <= ladder.bottomRight.x ); 
+    bool collideY = ( human.bottomLeft.y >= ladder.bottomLeft.y && ladder.topLeft.y >= human.bottomRight.y - 10);
     bool collided = collideX && collideY;
 
     /*
